043825ccdf1e097e83f46444fa927725f9faa059
victorino3 <victorinogomes1@gmail>
2022-12-20 01:11:33 +0000

    Creating dictionary to maximize the search

--- DB/word.js
+++ DB/word.js
@@ -2,8 +2,17 @@ import synonyms from 'synonyms';
 import WordPOS from 'wordpos'
 let wordpos = new WordPOS();
 const inputUser = "Mac Book Pro 16 16GB / 512GB M1 Pro CPU 10-core GPU 16-core / Prateado";
-let resultWord = wordpos.getNouns(inputUser,console.log)
-console.log(resultWord.entries())
+let resultWord = wordpos.getNouns(inputUser)
+resultWord
+.then((result) => {
+    for (const iterator of result) {
+        console.log(synonyms(iterator));
+    }
+}).catch((err) => {
+    
+});
+let keys = Object.keys(resultWord)
+
 /*for (const iterator of resultWord) {
     console.log(synonyms(iterator));
 }
