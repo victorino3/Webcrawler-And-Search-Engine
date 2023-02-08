b6d5430bc18c2f64d4789df010d3d0842ed3086f
victorino3 <victorinogomes1@gmail>
2023-02-08 14:13:00 +0000

    Test files

--- test.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-import {app} from "./test/index.txt"
-if(fs.existsSync("../")){
-    console.log("yes")
-}
-console.log("Not")
--- test/switch.test.mjs
+++ test/switch.test.mjs
@@ -3,22 +3,21 @@ import request from "supertest"
 import {app} from "../server.mjs"
 import assert from "assert"
 import fs from 'fs';
-const file = "index.txt"
 
 
-describe("API test for all case",()=>{
-    describe("/product/macbook-pro",()=>{
+
+describe("/product/macbook-pro",()=>{
         it("Upload page without error", async ()=>{
             const response = await request(app)
                             .get("/product/macbook-pro")
                             .expect(200)
-
-            fs.readFile("../test/index.txt", 'utf-8', (err, data) => {
-                if (err) throw err;
-                //assert.deepEqual(response.txt,data)
-                console.log(data)
-              });
-            
+            if(fs.existsSync("/home/victorino/Documents/JsExpert/Webcrawler-And-Search-Engine/test/index.txt")){
+                            fs.readFile("/home/victorino/Documents/JsExpert/Webcrawler-And-Search-Engine/test/index.txt", 'utf-8', (err, data) => {
+                                if (err) throw err;
+                                   assert.deepStrictEqual(response.text,data);
+                                  });
+                            }else{
+                            console.log("Not")
+                            }
         })
-    })
 })
