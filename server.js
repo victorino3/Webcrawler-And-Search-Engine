f408765d0077caba67def82174e718abd0304e4e
victorino3 <victorinogomes1@gmail>
2023-02-15 13:19:30 +0000

    codings

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const cors = require("cors")
-const exphbs  = require('express-handlebars')
-const {run,completeSearch} = require("./DB/aggregation.js")
-const {fileURLToPath} = require('url');
+const exphbs = require('express-handlebars')
+const { run, completeSearch } = require("./DB/aggregation.js")
 const { join } = require("path")
-const path = require('path');
 const { config } = require("dotenv")
 const { ok } = require("assert")
-const env =process.env.NODE_ENV || "dev" && "prod"
-ok(env === "dev" || env === "prod" , "Invalid environment specified")
-const confiPath =join(__dirname, "./dotenv", `.env.${env}`)
+const env = process.env.NODE_ENV || "dev" && "prod"
+ok(env === "dev" || env === "prod", "Invalid environment specified")
+const confiPath = join(__dirname, "./dotenv", `.env.${env}`)
 config({
     path: confiPath,
 })
-const port =  process.env.PORT
+const port = process.env.PORT
 const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,43 +19,45 @@ app.use(express.json());
 app.use(cors())
 
 app.set('view engine', 'hbs');
-app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "index"}));
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "index" }));
 
-app.get('/product/macbook-pro', async(req, res) => {
-    try {
+
+app.get('/product/macbook-pro',(req, res) => {
         res.status(200)
         res.render('main');
-    } catch (error) {
-        console.log(error)
-        res.status(400)
-        res.render('error');
-    }
-    
-});
 
-app.post('/product/search', async(req, res) => {
+});
+app.get('/product/macbook-proX',(req,res)=> {
+    
+        res.status(400);
+        res.render('error'); 
+   
+  });
+
+
+app.post('/product/search', async (req, res) => {
     try {
         const params = req.body
-        if(!params.autoSearch){
+        if (!params.autoSearch) {
             return res.render('error');
         }
-        let data = await run(params.autoSearch) 
-        res.render('main',{data});
+        let data = await run(params.autoSearch)
+        res.render('main', { data });
     } catch (error) {
         console.log(error)
         res.render('error');
     }
 });
 
-app.get('/auto-search',async (req, res) => {
-    let data = await completeSearch(req.query.term) 
-        var DataList = []
-        for (let i = 0; i < data.length; i++) {
-          DataList.push(data[i].title)
-        }
-        res.end(JSON.stringify(DataList))
-      },
-    
+app.get('/auto-search', async (req, res) => {
+    let data = await completeSearch(req.query.term)
+    var DataList = []
+    for (let i = 0; i < data.length; i++) {
+        DataList.push(data[i].title)
+    }
+    res.end(JSON.stringify(DataList))
+},
+
 )
 
 app.listen(port, console.log("listening on port", port))
--- test/switch.test.js
+++ test/switch.test.js
@@ -1,9 +1,12 @@
 const  { it,describe } = require("mocha")
 const request = require("supertest")
 const app = require("../server.js")
+const { join } = require("path")
 const assert = require("assert")
 const {readFileSync} = require('fs');
-let file = "/home/victorino/Documents/JsExpert/Webcrawler-And-Search-Engine/test/index.txt"
+const confiPath =join(__dirname,"index.txt")
+const confiPathError =join(__dirname,"error.txt")
+//let file = "/home/victorino/Documents/JsExpert/Webcrawler-And-Search-Engine/test/index.txt"
 
 describe("Test Switch",()=>{
     describe("/product/macbook-pro",()=>{
@@ -11,10 +14,28 @@ describe("Test Switch",()=>{
             const response = await request(app)
                             .get("/product/macbook-pro")
                             .expect(200)
-            const expected = readFileSync(file, "utf-8")
-                            
+            const expected = readFileSync(confiPath, "utf-8")          
             assert.deepStrictEqual(response.text,expected);
             });
+
+        it("Cacth the error if get some error",async ()=>{
+            const response = await request(app)
+                            .get("/product/macbook-proX")
+                            .expect(400)
+            const expected = readFileSync(confiPathError, "utf-8")
+            assert.deepStrictEqual(response.text,expected);
+        })
+
+        it("track user input in search field", async()=>{
+            const expect = "mac"
+            const response = await request(app)
+                            .get("http://localhost:3000/auto-search?key=%QUERY")
+                            .expect((req)=>{
+                                console.log(req.query)
+                            })
+            
+            //assert.deepStrictEqual(response.text,expected);
+        })
                          
  })
 
